cf2631d5e6702273c97bad94a1e18a678375806a
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2023-04-20 21:51:27 -0500

    deleting logs

--- script.js
+++ script.js
@@ -6,54 +6,47 @@ var specialCharacters = ['!', '#','$', '%', '&', '*', '(',')','+','-', ',', '.',
 var upperCase = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 9, 0];
-console.log(specialCharacters);
-console.log(upperCase);
-console.log(lowerCase);
-console.log(numbers);
+
 
 
 function generatePassword(){
   //prompts with password length from user
   var minLength =Number( window.prompt('Please, select the password criteria. \nWhat is the minimum length of the password?' ));
-  console.log(minLength); 
+ 
   
    while (minLength<8){
      window.alert("The password length can't be less than 8 characters. Please try again!")
      var minLength =Number( window.prompt('Please, select the password criteria. \nWhat is the minimum length of the password?' ));
-      console.log(minLength); 
       
     } 
  
   
   var maxLength = Number( window.prompt (' Now , what about maximum length?') );
-  console.log(maxLength);
+ 
   while (maxLength>128) {
     window.alert("The password length can not exceed 128 characters. Try again!");
     var maxLength = Number( window.prompt (' Now , what about maximum length?') );
-  console.log(maxLength);
+  
   }
 
   
 
   // calculating password length
   var randomPlus = Math.round(Math.random()* (maxLength-minLength));
-  console.log(randomPlus);
+  
   var passwordLength = randomPlus + minLength;
-  console.log('the password length is ', passwordLength);
+  
 
 
   //promts about presents of upper, lower case letters and special characters
 
   var upper = window.confirm ('How about uppercase letters?');
-  console.log(upper);
-
   var lower = window.confirm ('Lowercase may be?');
   var number = window.confirm ('What about numbers?');
 
   var special = window.confirm (' Do you want special characters?');
-  console.log(special);
-
-  //alert if everythng is false
+  
+  //alert if everything is false
   if (upper==false && lower==false && special==false && number==false) {
     window.alert('Try again!');
   }
@@ -85,14 +78,10 @@ function generatePassword(){
 
   if (upper) {
     upperLength();
-    
-  } else {
+   } else {
     upperArrayLength = 0;
-  //let upperArray = [];
   }
-
-  console.log('uppercase letters array : ',upperArray);
-
+ 
   //function for lowercase part
   function lowerLength() {
     lowerArrayLength = Math.round(Math.random() * passwordLength);
@@ -108,18 +97,14 @@ function generatePassword(){
     
   }
 
-
   if (lower) {
     lowerLength();
   } else {
     lowerArrayLength=0;
-    //let lowerArray= [];
+    
   }
-
-  console.log('lowercase letters array : ',lowerArray); 
-
+  
   //function for numbers
-
   function numbersLength() {
     numbersArrayLength = Math.round(Math.random() * passwordLength);
     if (numbersArrayLength>=1 && numbersArrayLength<=(passwordLength-upperArrayLength-lowerArrayLength-1)) {
@@ -137,9 +122,9 @@ function generatePassword(){
     numbersLength();
   } else {
     numbersArrayLength=0;
-    
+   
   }
-   console.log ('numbers array: ', numbersArray)
+   
 
    //special characters
   function specialLength() {
@@ -156,13 +141,10 @@ function generatePassword(){
 
   } else {
     specialArrayLength=0;
-  // let specialArray=[];
   }
-  console.log(' special characters array: ',specialArray);
-
+  
   //merging arrays
-
-  var passwordArray= upperArray.concat(specialArray, lowerArray, numbersArray);
+   var passwordArray= upperArray.concat(specialArray, lowerArray, numbersArray);
   console.log(passwordArray);
 
   //shuffle elements in the password array
@@ -170,11 +152,11 @@ function generatePassword(){
   for (i=1; i<=shuffle; i++){
   passwordArray.sort(function(){return 0.5 - Math.random()});
   }
-  console.log(passwordArray);
+  
   var password = passwordArray.join("");
-  console.log(password);
   return password;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
